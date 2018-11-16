1b9cd4723063518db26000f6480070f9ffd7545d
akkk33 <akkk33@protonmail.com>
2018-11-16 04:06:10 +0200

    Implemented basic unittest

--- Araneus/preferences.py
+++ Araneus/preferences.py
@@ -1,6 +1,29 @@
-class Create():
+import os, json
+from pathlib import Path
+
+conf_dir = str(Path.home()) + "/.config/Araneus/"
+conf_file = conf_dir + "conf.json"
+
+
+class Create:
     pass
 
 
-class Read():
-    pass
+class Read:
+    def __init__(self):
+        if not self.check_file_exists():
+            Create()
+        else:
+            self.parse()
+
+    def check_file_exists(self):
+        if os.path.isfile(conf_file):
+            return True
+
+    def parse(self):
+        return json.load(conf_file)
+
+
+class Modify:
+    def __init__(self):
+        pass
