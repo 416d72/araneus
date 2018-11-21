09f9a25f1ab308e57182b3fd975e47e78241038e
akkk33 <akkk33@protonmail.com>
2018-11-22 01:01:19 +0200

    Finished basic preferences class with ability to read, write configurations.

--- Araneus/preferences.py
+++ Araneus/preferences.py
@@ -1,61 +1,97 @@
-import json
+import configparser
 import os
 import sys
 from shutil import copy2
-from pathlib import Path
 
-conf_dir = str(Path.home()) + "/.config/Araneus/"
-conf_file = conf_dir + "conf.json"
-std_conf_file = str(os.getcwd() + os.sep + 'configurations' + os.sep + 'default-conf.json')
+config = configparser.ConfigParser()
 
 
 def check_os():
     """
-    Checks if the current operating system is only Linux so that the app don't fail with other filesystems
+    Checks if the current operating system is only Linux so that the app won't fail with others
     :return: Bool
     """
     if sys.platform.startswith('linux'):
         return True
 
 
-def create_dir():
+class Preferences:
     """
-    Creates Application's directory
-    :return: Bool
+    Contains all methods for manipulate settings
     """
-    if not os.path.exists(conf_dir):
-        try:
-            os.mkdir(conf_dir)
+
+    def __init__(self):
+        """
+        - Checking os platform first
+        - Setting file paths
+        """
+        self.conf_dir = str(os.path.expanduser("~")) + "/.config/Araneus/"
+        self.conf_file_path = self.conf_dir + "conf.ini"
+        self.std_conf_file_path = str(os.path.dirname(os.getcwd()) + "/Araneus/" + '/configurations/' +
+                                      'default-conf.ini')
+        self.create_config_file()
+
+    def create_config_dir(self):
+        """
+        Creates Application's directory
+        :return: Bool
+        """
+        if not os.path.exists(self.conf_dir):
+            try:
+                os.mkdir(self.conf_dir)
+                return True
+            except ValueError:
+                return ValueError
+        else:
             return True
-        except ValueError:
-            return ValueError
 
-
-def create_file():
-    """
-    Creates configurations file
-    :return: Bool
-    """
-    if not os.path.isfile(conf_file):
-        try:
-            copy2(std_conf_file, conf_file)
+    def create_config_file(self):
+        """
+        Creates configurations file
+        :return: Bool
+        """
+        if not os.path.isfile(self.conf_file_path):
+            try:
+                self.create_config_dir()
+                copy2(self.std_conf_file_path, self.conf_file_path)
+                return True
+            except ValueError:
+                return ValueError
+        else:
             return True
+
+    def reset(self):
+        """
+        Resets preferences by copying the original configurations file over the old one
+        :return: None
+        """
+        try:
+            self.create_config_file()
+            copy2(self.std_conf_file_path, self.conf_file_path)
         except ValueError:
-            return ValueError
+            return "Couldn't reset settings!"
 
+    def validate(self, section, option):
+        config.read(self.conf_file_path)
+        if not config.has_option(section, option):
+            self.reset()
 
-def parse():
-    """
-    Read configurations file
-    :return: String|JSON
-    """
-    try:
-        create_dir()
-        create_file()
-        return json.load(conf_file)
-    except ValueError:
-        return ValueError
+    def get_option(self, section, option):
+        """
+        Read configurations file
+        :return: String
+        """
+        try:
+            self.validate(section, option)
+            return config[section][option]
+        except ValueError:
+            return ValueError("Error occurred while getting the required option value")
 
-
-def __main__():
-    pass
+    def set_option(self, section, option, value):
+        try:
+            self.validate(section, option)
+            config[section][option] = value
+            with open(self.conf_file_path, 'w') as f:
+                config.write(f)
+        except ValueError:
+            raise ValueError("Error occurred while changing preferences")
--- tests/preferences_test.py
+++ tests/preferences_test.py
@@ -1,7 +1,16 @@
 import unittest
-import json
+
 from Araneus.preferences import *
 
 
-def test_os():
-    assert check_os() is True
+class TestPreferences(unittest.TestCase):
+
+    def test_create_config_dir(self):
+        self.assertEqual(Preferences().create_config_dir(), True)
+
+    def test_create_config_file(self):
+        self.assertEqual(Preferences().create_config_file(), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
