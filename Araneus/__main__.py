f7b3549ff04c4d6fb484dcea8685172292461c00
akkk33 <akkk33@protonmail.com>
2018-11-24 02:56:08 +0200

    Beginning with main window

--- Araneus/__main__.py
+++
@@ -1,5 +0,0 @@
-from Araneus.preferences import *
-
-print(Preferences().get_option("GENERAL", "LANGUAGE"))
-Preferences().set_option("GENERAL", "LANGUAGE", 'en')
-print(Preferences().get_option("GENERAL", "LANGUAGE"))
