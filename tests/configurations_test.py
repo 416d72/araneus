9aed781510605fd648f86cd37f1b3ed6989d9a95
akkk33 <akkk33@protonmail.com>
2018-11-25 07:47:35 +0200

    Finished basic tests

--- tests/configurations_test.py
+++ tests/configurations_test.py
@@ -2,32 +2,28 @@ import unittest
 
 from Araneus.configurations import *
 
-configurations = Configurations()
-
-print(os.path.dirname(__file__))
-
 
 class TestPreferences(unittest.TestCase):
     def test_declare(self):
-        self.assertTrue(configurations.declare())
+        self.assertTrue(Configurations().declare())
 
     def test_create_config_dir(self):
-        self.assertTrue(configurations.create_config_dir())
+        self.assertTrue(Configurations().create_config_dir())
 
     def test_create_config_file(self):
-        self.assertTrue(configurations.create_config_file())
+        self.assertTrue(Configurations().create_config_file())
 
     def test_reset(self):
-        self.assertTrue(configurations.reset())
+        self.assertTrue(Configurations().reset())
 
     def test_validate(self):
-        self.assertTrue(configurations.validate('GENERAL', 'Language'))
+        self.assertTrue(Configurations().validate('GENERAL', 'Language'))
 
     def test_get_option(self):
-        self.assertIsInstance(configurations.get_option('GENERAL', 'Language'), str)
+        self.assertIsInstance(Configurations().get_option('GENERAL', 'Language'), str)
 
     def test_set_option(self):
-        self.assertIsInstance(configurations.set_option('GENERAL', 'Language', 'en'), str)
+        self.assertIsInstance(Configurations().set_option('GENERAL', 'Language', 'en'), str)
 
 
 if __name__ == '__main__':
---
+++ tests/helpers_test.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+# -*- coding: utf-8; -*-
+# LICENSE: see Araneus/LICENSE
+import unittest
+from Araneus.helpers import *
+
+
+class TestHelpers(unittest.TestCase):
+    def test_is_running(self):
+        self.assertFalse(is_running())
+
+    def test_load_ui(self):
+        self.assertIsInstance(load_ui('about'), str)
