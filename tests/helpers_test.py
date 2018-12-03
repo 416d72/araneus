7d34baf9a511b1f27ca8e76cd2d0160ebc71a985
akkk33 <akkk33@protonmail.com>
2018-12-03 10:04:49 +0200

    Updated the way to view items from show containing directory to view items directly.

--- tests/helpers_test.py
+++ tests/helpers_test.py
@@ -6,7 +6,7 @@ from Araneus.helpers import *
 
 class TestHelpers(unittest.TestCase):
     def test_is_running(self):
-        self.assertIsInstance(is_running(), IOError)
+        self.assertTrue(is_running())
 
     def test_load_ui(self):
         self.assertIsInstance(load_ui('about'), str)
