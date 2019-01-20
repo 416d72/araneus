bc56c251636d59f360700ae1bc0f96be50c7e864
akkk33 <akkk33@protonmail.com>
2019-01-21 01:02:30 +0200

    Implemented merge sort algorithm

--- Araneus/mlite/main.c
+++ Araneus/mlite/main.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int tblsum(int *table,int table_size);
-float tblavg(int *table,int table_size);
+#include "tables.h"
+
 int main()
 {
-    srand(time(NULL));
-    int index = 130;
-    int table[index];
-    for (int i = 0;i<index;i++)
-    {
-        table[i] = rand()%(100);
-    }
-    int sum = tblsum(table,index);
-    float avg = tblavg(table,index);
-    printf("Table has %d rows:\nsum: %d\nAverage: %.2f\n",index,sum,avg);
+    // srand(time(NULL));
+    
+    int table[] = {23,54,13,2,83,76,20,69};
+    
+    // for (int i = 0;i<50;i++)
+    // {
+    //     table[i] = rand()%(100);
+    // }
+    
+    int size = sizeof(table);
+    
+    // int sum = tblsum(table,index);
+    // float avg = tblavg(table,index);
+    // printf("Array has %d elements:\n", index);
+    // printf("sum: %d\nAverage: %.2f\n", sum,avg);
+    
+    printf("Size of the array= %d elements\n", size);
     return 1;
-}
-int tblsum(int *table,int table_size)
-{
-    int result = 0;
-    for (int i = 0; i < table_size; i++)
-    {
-        result += table[i];
-    }
-    return result;
-}
-float tblavg(int *table,int table_size)
-{
-    float result = 0;
-    for (int i = 0; i < table_size; i++)
-    {
-        result += table[i];
-    }
-    return result / table_size;
 }
