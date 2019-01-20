ea6abe68b8aad7f044408886e982a9d72d2a8b6e
akkk33 <akkk33@protonmail.com>
2019-01-20 02:14:08 +0200

    Just learnt pointers <Awesome>

--- Araneus/mlite/main.c
+++ Araneus/mlite/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//int square(int value);
 int main()
 {
-	printf("Hello from vscode\n");
+    // int blue = square(15);
+    // printf("Squared: %d\n", blue);
+    int table[13];
+    printf("Table starts at %d\n",table);
     return 0;
 }
+
+// int square(int value)
+// {
+//     return value * value;
+// }
