200159e9fdf5d2247009d3f9c463c933ab3113bc
akkk33 <akkk33@protonmail.com>
2019-01-21 05:46:49 +0200

    Recursive functions

--- Araneus/mlite/main.c
+++ Araneus/mlite/main.c
@@ -2,25 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include "tables.h"
+#include "recursive.h"
 
 int main()
 {
     // srand(time(NULL));
     
-    int table[] = {23,54,13,2,83,76,20,69};
+    // int table[] = {23,54,13,2,83,76,20,69};
     
     // for (int i = 0;i<50;i++)
     // {
     //     table[i] = rand()%(100);
     // }
     
-    int size = sizeof(table);
+    // int size = sizeof(table);
     
     // int sum = tblsum(table,index);
     // float avg = tblavg(table,index);
     // printf("Array has %d elements:\n", index);
     // printf("sum: %d\nAverage: %.2f\n", sum,avg);
     
-    printf("Size of the array= %d elements\n", size);
+    // printf("Size of the array= %d elements\n", size);
+    // Factorial
+    printf("Type a number: ");
+    int num;
+    scanf("%d", &num);
+    printf("Factorial of %d is %d\n",num, factorial(num));
     return 1;
 }
--- Araneus/mlite/merge_sort.c
+++ Araneus/mlite/merge_sort.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-void merge(int *arr, int l, int m, int r)
+#include <time.h>
+
+void merge(int *arr, int left, int middle, int right)
 {
     int i, j, k; 
-    int n1 = m - l + 1; 
-    int n2 =  r - m; 
+    int n1 = middle - left + 1; 
+    int n2 =  right - middle; 
   
     /* create temp arrays */
     int L[n1], R[n2]; 
   
     /* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++) 
-        L[i] = arr[l + i]; 
+        L[i] = arr[left + i]; 
     for (j = 0; j < n2; j++) 
-        R[j] = arr[m + 1+ j]; 
+        R[j] = arr[middle + 1+ j]; 
   
     /* Merge the temp arrays back into arr[l..r]*/
     i = 0; // Initial index of first subarray 
     j = 0; // Initial index of second subarray 
-    k = l; // Initial index of merged subarray 
+    k = left; // Initial index of merged subarray 
     while (i < n1 && j < n2) 
     { 
         if (L[i] <= R[j]) 
@@ -52,18 +54,26 @@ void merge(int *arr, int l, int m, int r)
         k++; 
     } 
 }
-void split(int *arr, int l, int r)
+void split(int *arr, int left, int right)
 {
-    if (l < r)
+    if (left < right)
     {
-        int m = l + (r - l) / 2;
-        split(arr,l,m);
-        split(arr,m+1,r);
-        merge(arr,l,m,r);
+        int middle = left + (right - left) / 2;
+        split(arr,left,middle);
+        split(arr,middle+1,right);
+        merge(arr,left,middle,right);
+    }
+}
+void fill(int *arr,int size)
+{
+    for (int i = 0;i<size;i++)
+    {
+        arr[i] = rand()%(100);
     }
 }
 void printArr(int *arr,int size)
 {
+    // printf("\n");
     for (int i = 0;i < size; i++)
     {
         printf("%d ",arr[i]);
@@ -72,10 +82,14 @@ void printArr(int *arr,int size)
 }
 int main()
 {
-    int arr[] = {8,21,4,1,52,92,13,63};
-    int size = sizeof(arr)/sizeof(arr[0]);
+    srand(time(NULL));
+    int size = 10;
+    int arr[size];
+    // int size = sizeof(arr)/sizeof(arr[0]);
+    fill(arr,size);
     printArr(arr,size);
     split(arr,0,size-1);
+    printf("\n");
     printArr(arr,size);
     return 1;
 }
