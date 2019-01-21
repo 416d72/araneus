6cc0aa9b2cffc2d81fdf7f9cdbac18de7027f2e4
akkk33 <akkk33@protonmail.com>
2019-01-21 20:45:06 +0200

    Beginning with file details fetching

--- Araneus/mlite/mlite.c
+++ Araneus/mlite/mlite.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
-#include <magic.h>
+#include <sys/types.h>
 
-int main()
+
+void details(struct stat st,const char *file_name)
 {
-    const char *file_name = "pointers";
-    
-    struct stat st;
     stat(file_name,&st);
 
     printf("Size: %ld \n",st.st_size);
     printf("Modified: %ld \n",st.st_mtime);
     printf("Accessed: %ld \n",st.st_atime);
     printf("Type (int): %d \n",st.st_mode);
+}
+int main()
+{
+    const char *file_name = "pointers";
+    struct stat st;
     
-    // Magic
-    const char *mime;
-    magic_t magic;
-    magic = magic_open(MAGIC_MIME_TYPE);
-    magic_load(magic,NULL);
-    magic_compile(magic,NULL);
-    mime = magic_file(magic,file_name);
-    printf("Type: %s \n",mime);
-    magic_close(magic);
-    return 0;
+    details(st,file_name);
+
+    return 1;
 }
