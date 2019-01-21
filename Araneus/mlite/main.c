fa9a38df92958a9be2dea1767be6fb4bf8ee9318
akkk33 <akkk33@protonmail.com>
2019-01-21 07:59:36 +0200

    Getting file meta data like size, modification time, access time.. Trying to get MIME type..

--- Araneus/mlite/main.c
+++ Araneus/mlite/main.c
@@ -3,30 +3,39 @@
 #include <time.h>
 #include "tables.h"
 #include "recursive.h"
-
+void test_factorial();
+void test_basic_arrays();
 int main()
 {
-    // srand(time(NULL));
-    
-    // int table[] = {23,54,13,2,83,76,20,69};
-    
-    // for (int i = 0;i<50;i++)
-    // {
-    //     table[i] = rand()%(100);
-    // }
-    
-    // int size = sizeof(table);
-    
-    // int sum = tblsum(table,index);
-    // float avg = tblavg(table,index);
-    // printf("Array has %d elements:\n", index);
-    // printf("sum: %d\nAverage: %.2f\n", sum,avg);
-    
-    // printf("Size of the array= %d elements\n", size);
+    char name = 'A';
+    printf("%c \n",name);
+    return 1;
+}
+void test_factorial()
+{
     // Factorial
     printf("Type a number: ");
     int num;
     scanf("%d", &num);
     printf("Factorial of %d is %d\n",num, factorial(num));
-    return 1;
+}
+void test_basic_arrays()
+{
+    srand(time(NULL));
+    int index = 50;
+    int table[index];
+    
+    for (int i = 0;i<index;i++)
+    {
+        table[i] = rand()%(100);
+    }
+    
+    int size = sizeof(table);
+    
+    int sum = tblsum(table,index);
+    float avg = tblavg(table,index);
+    printf("Array has %d elements:\n", index);
+    printf("sum: %d\nAverage: %.2f\n", sum,avg);
+    
+    printf("Size of the array= %d elements\n", size);
 }
